6ced24968521c6a7ce8f5a1c2b1f73b6911e67df
bcround <sbc1142@gmail.com>
2022-07-07 22:11:05 +0900

    Chapter 2 Done. - onPress - implement simpe counter example - how to use StyleSheet (styling)

--- App.js
+++ App.js
@@ -5,18 +5,27 @@
  * @format
  */
 
-import React from 'react';
-// import type {Node} from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import Counter from './components/Counter';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const onIncrease = () => setCount(count + 1);
+  const onDecrease = () => setCount(count - 1);
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello React Native!</Text>
-      </View>
+    <SafeAreaView style={styles.full}>
+      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+  },
+});
 
 export default App;
